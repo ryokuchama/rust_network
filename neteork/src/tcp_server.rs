cba756492674d059f35bfd0605322be47352aa72
ryokuchama <r.madrid.pg@gmail.com>
2023-02-11 18:28:51 +0900

    once commit.

--- neteork/src/tcp_server.rs
+++ neteork/src/tcp_server.rs
@@ -5,8 +5,11 @@ use std::{str, thread};
 use log::debug;
 
 pub trait IEchoTcpServer {
+    // difference between self and Self
+    // self: use for first arg of func
+    // Self: type of the current object.
     fn serve(&self) -> Result<(), failure::Error>;
-    fn handler(&self, mut stream: TcpStream) -> Result<(), failure::Error>;
+    fn handler(&self, stream: TcpStream) -> Result<(), failure::Error>;
 }
 
 pub struct EchoTcpServer {
@@ -14,20 +17,29 @@ pub struct EchoTcpServer {
 }
 
 impl IEchoTcpServer for EchoTcpServer {
+    // wait for imcoming connection with specific IP address
     fn serve(&self) -> Result<(), failure::Error> {
-        let listener = TcpListener::bind(self.ip_address)?;
+        // bound to specific IP address
+        // it is nessesary to pass ref to bind().
+        let listener = TcpListener::bind(&self.ip_address)?;
         // loop=while true. little bit faster than while
         // because conditional is not checked every time.
         loop {
+            // accept incoming connection from client
             let (stream, _) = listener.accept()?;
             thread::spawn(move || {
-                self.handler(stream)
-                    .unwrap_or_else(|error| error!("{:?}", error));
+                self
+                    .handler(stream)
+                    .unwrap_or_else(|error| log::error!("{:?}", error));
             });
         }
     }
 
-    // mut arg=mutable arg
+    // wait for connection from client
+    // and return same message on receiving the message
+    // mut arg=mutable arg.
+    // if trait func arg is not mut, impl func arg can be mut.
+    // impl func arg is for inside of func. it is not related o interface.
     fn handler(&self, mut stream: TcpStream) -> Result<(), failure::Error> {
         // peer_addr: Returns the socket address of the remote peer of this TCP connection.
         println!("Handling data from {}", stream.peer_addr()?);
